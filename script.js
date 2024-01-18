2ca052085abb1381f03e211eabc63cb1cea8abc0
Kunduz2305 <148209588+Kunduz2305@users.noreply.github.com>
2024-01-18 21:10:23 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let transSumEl = document.getElementById('currency')
+let mountEl = document.getElementById('mount')
+let sumEl = document.getElementById('sum')
+let resultEl = document.getElementById('result')
+
+const sumCurrent = () => {
+let transEl = transSumEl.value
+let mount = mountEl.value
+
+let resEl = transEl * mount
+
+// console.log(resEl)
+
+resultEl.innerHTML = 'Суммасы:' + resEl.toFixed(2)
+}
+sumEl.addEventListener('click',sumCurrent)
+// document.getElementById('result').innerHTML
